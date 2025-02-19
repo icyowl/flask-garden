3aa2f965174255887b005694d0e6320271e36b00
AkiraWin <frog7bar@gmail.com>
2025-02-20 00:04:28 +0900

    delete alice

--- init_db.py
+++ init_db.py
@@ -1,13 +1,7 @@
 import os
 import sqlite3
-from werkzeug.security import generate_password_hash
-
 
 def main():
-
-    user = 'Alice'
-    password = 'madteaparty1865'
-    pw_hash = generate_password_hash(password)
 
     conn = sqlite3.connect('instance/user.sqlite')
     cur = conn.cursor()
@@ -20,8 +14,6 @@ def main():
         username TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
         );
-
-        INSERT INTO user(username, password) VALUES('{user}', '{pw_hash}');
         """
     )
     conn.commit()
