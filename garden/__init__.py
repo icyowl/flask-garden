576c9e8de0260077f956b5b2f554e007c7ea0524
AkiraWin <frog7bar@gmail.com>
2025-02-19 15:38:48 +0900

    micro change

--- garden/__init__.py
+++ garden/__init__.py
@@ -31,7 +31,7 @@ def create_app():
     def index():
         if not 'user_id' in session:
             return redirect(url_for('auth.login'))
-        sk = app.config['SECRET_KEY']
+        sk = os.environ.pop('SECRET_KEY')
         return render_template('index.html', sk=sk)
 
     return app
