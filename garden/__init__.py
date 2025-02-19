1e55cdc732d7b26855b0f144a1e997bc4944848b
AkiraWin <frog7bar@gmail.com>
2025-02-19 15:01:26 +0900

    if dotenv isfile()

--- garden/__init__.py
+++ garden/__init__.py
@@ -4,18 +4,21 @@ from flask import Flask, render_template, redirect, url_for, session
 
 def create_app():
 
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-
     app = Flask(__name__)
+    # app.config['TESTING'] = True
+
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.isfile(dotenv_path):
+        load_dotenv(dotenv_path)
+        app.config['SECRET_KEY'] = os.environ.pop('SECRET_KEY')
+    else:
+        app.config['SECRET_KEY'] = 'dev'
 
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # app.config['TESTING'] = True
-    app.config['SECRET_KEY'] = os.environ.pop('SECRET_KEY')
     app.config['DATABASE'] = os.path.join(app.instance_path, 'user.sqlite')
 
     from . import db
